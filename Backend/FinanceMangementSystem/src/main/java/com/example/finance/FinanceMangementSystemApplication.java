9f1076634c4df0e0b2d258e8f056ca1f5818a54d
antara <iamantaraslg@gmail.com>
2022-08-26 11:54:19 +0530

    First commmit from Antara

---
+++ Backend/FinanceMangementSystem/src/main/java/com/example/finance/FinanceMangementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class FinanceMangementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceMangementSystemApplication.class, args);
+	}
+
+}
