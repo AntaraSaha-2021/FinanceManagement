9df0031643c0201a11f16095a415887885bbf389
anushkahalder <anushkahalder2@gmail.com>
2022-08-27 10:32:05 +0530

    Product Table update

---
+++ Backend/FinanceMangementSystem/src/main/java/com/example/finance/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.finance.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.finance.entity.Product;
+
+@Repository
+public interface ProductRepository  extends JpaRepository<Product, Integer> {
+
+}
